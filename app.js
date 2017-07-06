8928d6ad7255c4d05a1d5bf9a5bc0ceeb090a167
linxz <linxz@vip.qq.com>
2017-07-07 00:43:33 +0800

    完成所有预想的功能，提交发布 2.0.0 版本

--- app.js
+++ app.js
@@ -1,27 +1,10 @@
 //app.js
-var shareUrlId = ""
+
 App({
   onLaunch: function (res) {
-    // wx.getStorage({
-    //   key: 'caniuseData',
-    //   success: function(res) {
-    //     console.log(res.data)
-    //     console.log("缓存读取成功")
-    //   },
-    //   fail: function() {
-    //     console.log("缓存读取失败了！！！！！！！")
-    //   }
-    // }),
-    // console.log(res.query.queryKey)
-    shareUrlId = res.query
 
-    if(res.query == "" || res.query == undefined){
-      this.globalData.queryKey = ""
-    }else{
-      this.globalData.queryKey = res.query
-    }
   },
   globalData:{
-    queryKey: ""
+
   }
 })
